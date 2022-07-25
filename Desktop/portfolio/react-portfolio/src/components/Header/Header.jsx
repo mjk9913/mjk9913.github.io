087d98d509c911c788fa93a2b9dc1df2b2b8f326
mjk9913 <mjk9913@nyu.edu>
2022-07-25 12:30:10 +0900

    revamped website with react, need to fix reactability

---
+++ Desktop/portfolio/react-portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div class="header">
+      <div class="name-h"> Min Jie Kim </div>
+      <ul class="option">
+        <a href="#df"><li>about</li></a>
+        <a href="#df"><li>experiences</li></a>
+        <a href="#df"><li>projects</li></a>
+        <a href="#df"><li>contact</li></a>
+      </ul>
+    </div>  
+  )
+}
+
+export default Header
---
+++ Desktop/portfolio/react-portfolio/src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './nav.css'
+
+function Nav() {
+  return (
+    <div class="email">
+        <a href="mailto: mjk9913@nyu.edu">mjk9913@nyu.edu</a>
+        <div class="line"></div>
+    </div>
+  )
+}
+
+export default Nav
